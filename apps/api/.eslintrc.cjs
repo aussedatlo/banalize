84a51421f6b3a102dc06124f31140d175cb2bd64
Louis Aussedat <aussedat.louis@gmail.com>
2024-09-01 15:38:03 +0200

    ✅ (api): Add IPTableService tests

--- apps/api/.eslintrc.cjs
+++ apps/api/.eslintrc.cjs
@@ -1,4 +1,5 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@repo/eslint-config"],
+  ignorePatterns: ["jest.config.ts", "jest.setup.ts"],
 };
--- apps/api/src/services/firewall/IPTableService.test.ts
+++ apps/api/src/services/firewall/IPTableService.test.ts
@@ -1,1 +1,99 @@
-describe("IPTableService", () => {});
+import * as child_process from "child_process";
+import { Left, Right } from "purify-ts";
+import { IPTablesService } from "./IPTablesService";
+
+jest.mock("child_process");
+
+describe("IPTablesService", () => {
+  let spy: jest.SpyInstance;
+  let service: IPTablesService;
+  const ip = "192.168.1.1";
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    spy = jest.spyOn(child_process, "exec");
+    service = new IPTablesService();
+  });
+
+  describe("ban function", () => {
+    it("should ban an IP", async () => {
+      // GIVEN
+      spy.mockImplementation((command, cb) => cb(null, "", ""));
+
+      // WHEN
+      const result = await service.ban(ip);
+
+      // THEN
+      expect(result).toEqual(Right(true));
+      expect(spy).toHaveBeenCalledWith(
+        `iptables -A INPUT -s ${ip} -j REJECT --reject-with icmp-port-unreachable`,
+        expect.any(Function),
+      );
+    });
+
+    it("should return an error if the command fails", async () => {
+      // GIVEN
+      spy.mockImplementation((command, cb) => cb(new Error("error"), "", ""));
+
+      // WHEN
+      const result = await service.ban(ip);
+
+      // THEN
+      expect(result).toEqual(Left(new Error("error")));
+    });
+
+    it("should return an error if the command throws an error", async () => {
+      // GIVEN
+      spy.mockImplementation(() => {
+        throw new Error("error");
+      });
+
+      // WHEN
+      const result = await service.ban(ip);
+
+      // THEN
+      expect(result).toEqual(Left(new Error("error")));
+    });
+  });
+
+  describe("unban function", () => {
+    it("should unban an IP", async () => {
+      // GIVEN
+      spy.mockImplementation((command, cb) => cb(null, "", ""));
+
+      // WHEN
+      const result = await service.unban(ip);
+
+      // THEN
+      expect(result).toEqual(Right(true));
+      expect(spy).toHaveBeenCalledWith(
+        `iptables -D INPUT -s ${ip} -j REJECT --reject-with icmp-port-unreachable`,
+        expect.any(Function),
+      );
+    });
+
+    it("should return an error if the command fails", async () => {
+      // GIVEN
+      spy.mockImplementation((command, cb) => cb(new Error("error"), "", ""));
+
+      // WHEN
+      const result = await service.unban(ip);
+
+      // THEN
+      expect(result).toEqual(Left(new Error("error")));
+    });
+
+    it("should return an error if the command throws an error", async () => {
+      // GIVEN
+      spy.mockImplementation(() => {
+        throw new Error("error");
+      });
+
+      // WHEN
+      const result = await service.unban(ip);
+
+      // THEN
+      expect(result).toEqual(Left(new Error("error")));
+    });
+  });
+});
