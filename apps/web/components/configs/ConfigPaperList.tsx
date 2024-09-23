3e63b83316e69c5b4d3dd75ded797bbadce7f11a
Louis Aussedat <aussedat.louis@gmail.com>
2024-09-23 12:10:24 +0200

    🐛 (web): Fix stats when not computed

--- apps/web/components/configs/ConfigPaperList.tsx
+++ apps/web/components/configs/ConfigPaperList.tsx
@@ -3,6 +3,13 @@ import { Grid, GridCol } from "@mantine/core";
 import { ConfigPaper } from "components/configs/ConfigPaper";
 import { Config } from "types/Config";
 
+const DEFAULT_VALUE: StatsCountCollection = {
+  bansCount: 0,
+  matchesCount: 0,
+  currentBansCount: 0,
+  currentMatchesCount: 0,
+};
+
 type ConfigPaperListProps = {
   configs: Config[];
   stats: StatsCountCollection;
@@ -13,7 +20,10 @@ export const ConfigPaperList = ({ configs, stats }: ConfigPaperListProps) => {
     <Grid>
       {configs.map((config) => (
         <GridCol span={{ base: 12, xs: 4 }} key={config._id}>
-          <ConfigPaper config={config} stats={stats.data[config._id]} />
+          <ConfigPaper
+            config={config}
+            stats={stats.data[config._id] ?? DEFAULT_VALUE}
+          />
         </GridCol>
       ))}
     </Grid>
