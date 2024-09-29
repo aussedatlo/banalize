065ac5d9208709f13b0d328619a24c84a1540dd8
Louis Aussedat <aussedat.louis@gmail.com>
2024-09-29 13:37:33 +0200

    🏷️ (types/web): Add unknown  watcher status

--- apps/web/components/configs/ConfigPaperList.tsx
+++ apps/web/components/configs/ConfigPaperList.tsx
@@ -2,6 +2,7 @@ import {
   ConfigSchema,
   StatsSummary,
   StatsSummaryResponse,
+  WatcherStatus,
   WatcherStatusesResponse,
 } from "@banalize/types";
 import { Grid, GridCol } from "@mantine/core";
@@ -32,7 +33,7 @@ export const ConfigPaperList = ({
           <ConfigPaper
             config={config}
             stats={stats.data[config._id] ?? DEFAULT_VALUE}
-            status={statuses.data[config._id].status ?? "unknown"}
+            status={statuses.data[config._id].status ?? WatcherStatus.UNKNWOWN}
           />
         </GridCol>
       ))}
