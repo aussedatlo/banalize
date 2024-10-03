600900b34e3183052d213a3cd5d49601df9aaceb
Jason Deroubaix <jason.deroubaix@gmail.com>
2024-10-03 10:19:31 +0200

    🚧 (web): WIP timeline

--- apps/web/components/shared/Timeline/Timeline.tsx
+++ apps/web/components/shared/Timeline/Timeline.tsx
@@ -1,23 +1,29 @@
+"use client";
+
 import { BanSchema, MatchSchema } from "@banalize/api";
-import { Group } from "@mantine/core";
+import { ActionIcon, Group } from "@mantine/core";
+import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 import cx from "clsx";
+import { useRef, useState } from "react";
 import classes from "./Timeline.module.css";
+
+const EVENTS_TO_SHOW = 5;
+
 export type TimelineEvent = {
   date: Date;
   type: "match" | "ban" | "unban";
   event: MatchSchema | BanSchema;
 };
 
-// type EventProps = {
-//   timelineEvent: TimelineEvent;
-//   type: "match" | "ban";
-// };
-
 type TimelineProps = {
   events: TimelineEvent[];
 };
 
 export function Timeline({ events }: TimelineProps) {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [startIndex, setStartIndex] = useState(0);
+  const timelineRef = useRef<HTMLDivElement>(null);
+
   const getGradientClass = (
     currentEvent: TimelineEvent,
     nextEvent: TimelineEvent | undefined,
@@ -45,82 +51,116 @@ export function Timeline({ events }: TimelineProps) {
 
   const firstEvent = events[0];
   const lastEvent = events[events.length - 1];
-  const firstBan = events.find((event) => event.type === "ban");
-  const displayedEvents = [firstEvent, firstBan, lastEvent].filter(
-    (event) => event !== undefined,
+
+  const bansAndUnbans = events.filter(
+    (event) => event.type === "ban" || event.type === "unban",
+  );
+  const displayedEvents = [firstEvent, ...bansAndUnbans, lastEvent].filter(
+    (event, index, self) =>
+      event !== undefined && self.indexOf(event) === index,
   );
 
-  // copy an event for a test
-  const copyEvent = { ...firstEvent };
-  copyEvent.type = "unban";
+  const visibleEvents = displayedEvents.slice(
+    startIndex,
+    startIndex + EVENTS_TO_SHOW,
+  );
 
-  displayedEvents.push(firstEvent);
-  displayedEvents.push(lastEvent);
-  displayedEvents.push(copyEvent);
-  displayedEvents.push(lastEvent);
-  displayedEvents.push(firstEvent);
+  const handlePrevious = () => {
+    setStartIndex(Math.max(0, startIndex - 1));
+  };
+
+  const handleNext = () => {
+    setStartIndex(
+      Math.min(startIndex + 1, displayedEvents.length - EVENTS_TO_SHOW),
+    );
+  };
 
   return (
     <Group h={"100%"} w={"100%"}>
-      <div id={"timeline"} className={classes.timeline}>
-        {displayedEvents.map((item, index) => {
-          if (!item) return null;
-          const isTop = index % 2 !== 0;
-          const nextEvent = displayedEvents[index + 1];
-          const hasIntermediateEvents =
-            nextEvent &&
-            events.findIndex((e) => e === item) !==
-              events.findIndex((e) => e === nextEvent) - 1;
-          const numberOfIntermediateEvents = events.filter(
-            (e) =>
-              e.date > item.date &&
-              e.date < nextEvent?.date &&
-              e !== item &&
-              e !== nextEvent,
-          ).length;
+      <ActionIcon
+        color={"yellow"}
+        onClick={handlePrevious}
+        disabled={startIndex === 0}
+      >
+        <IconChevronLeft size={24} />
+      </ActionIcon>
+      <div ref={timelineRef} className={classes.timelineContainer}>
+        <div id={"timeline"} className={classes.timeline}>
+          {visibleEvents.map((item, index) => {
+            if (!item) return null;
+            const isTop = index % 2 !== 0;
+            const nextEvent = displayedEvents[index + 1];
+            const isLastEvent = index === visibleEvents.length - 1;
 
-          console.log('hasIntermediateEvents:', hasIntermediateEvents);
+            const intermediateEvents = events.filter(
+              (e) =>
+                e.date > item.date &&
+                (nextEvent ? e.date < nextEvent.date : true) &&
+                e !== item &&
+                e !== nextEvent,
+            );
 
-          const text = (
-            <div>
-              <span>
-                {/* First char uppercase */}
-                {item.type.charAt(0).toUpperCase() + item.type.slice(1)}
-              </span>
-              <span className={classes.date}>
-                {item.date.toDateString()}
-                <span></span>
-              </span>
-            </div>
-          );
-          const gradientClass = getGradientClass(item, nextEvent);
-
-          const concatClasses = cx([classes.bottom], {
-            [classes.lastBottom]: index === displayedEvents.length - 1,
-            [classes.isBan]: item.type === "ban",
-            [classes.isUnban]: item.type === "unban",
-            [gradientClass]: true,
-            [classes.noBar]: index === displayedEvents.length - 1,
-          });
-
-          return (
-            <div key={index} className={cx(classes.swiperSlide)}>
-              <div className={classes.top}>{isTop ? text : null}</div>
-
-              <div className={concatClasses}>
-                {!isTop ? text : <div />}
-                {hasIntermediateEvents ? (
-                  <div className={classes.intermediateEvents}>
-                    {numberOfIntermediateEvents}
-                  </div>
-                ) : (
-                  <div />
-                )}
+            const text = (
+              <div>
+                <span>
+                  {item.type.charAt(0).toUpperCase() + item.type.slice(1)}
+                </span>
+                <span className={classes.date}>{item.date.toDateString()}</span>
               </div>
-            </div>
-          );
-        })}
+            );
+
+            const concatClasses = cx([classes.bottom], {
+              [classes.lastBottom]: isLastEvent,
+              [classes.isBan]: item.type === "ban",
+              [classes.isUnban]: item.type === "unban",
+              [classes.noBar]: isLastEvent,
+            });
+
+            return (
+              <div key={index} className={cx(classes.swiperSlide)}>
+                <div className={classes.top}>{isTop ? text : null}</div>
+                <div className={concatClasses}>
+                  {!isTop ? text : <div />}
+                  {!isLastEvent && (
+                    <div
+                      className={cx(
+                        classes.eventLink,
+                        getGradientClass(item, nextEvent),
+                        {
+                          [classes.noHover]: hoveredIndex !== index,
+                          [classes.gotHover]: intermediateEvents.length > 0,
+                        },
+                      )}
+                      onMouseEnter={() => setHoveredIndex(index)}
+                      onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                      {intermediateEvents.length > 0 &&
+                        hoveredIndex === index && (
+                          <div className={classes.intermediateEvents}>
+                            +{intermediateEvents.length} events :<br />
+                            {
+                              intermediateEvents.filter(
+                                (e) => e.type === "match",
+                              ).length
+                            }{" "}
+                            match(es) <br />
+                          </div>
+                        )}
+                    </div>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
+      <ActionIcon
+        onClick={handleNext}
+        color={"yellow"}
+        disabled={startIndex + EVENTS_TO_SHOW >= displayedEvents.length}
+      >
+        <IconChevronRight size={24} />
+      </ActionIcon>
     </Group>
   );
 }
