31b8eb95846c5846b75f6fe770a482f7d0df9347
Louis Aussedat <aussedat.louis@gmail.com>
2024-10-10 22:23:02 +0200

    ♻️ (web): Add cache to useIpInfos hook

--- apps/web/app/hooks/useIpInfos.ts
+++ apps/web/app/hooks/useIpInfos.ts
@@ -9,13 +9,31 @@ const EMPTY_IP_INFOS: Partial<IpInfosResponse> = {
   location: undefined,
 };
 
+// In-memory cache object
+const ipInfoCache: Record<string, Partial<IpInfosResponse>> = {};
+
 export const useIpInfos = (ip: string) => {
   const [state, setState] = useState<Partial<IpInfosResponse>>(EMPTY_IP_INFOS);
 
   useEffect(() => {
-    fetch(`/api/ip-infos/${ip}`)
-      .then((response) => response.json())
-      .then((data) => setState(data));
+    if (ipInfoCache[ip]) {
+      setState(ipInfoCache[ip]);
+    } else {
+      setState(EMPTY_IP_INFOS);
+      // If not cached, fetch from the API
+      fetch(`/api/ip-infos/${ip}`)
+        .then((response) => response.json())
+        .then((data: Partial<IpInfosResponse>) => {
+          // Store the result in the cache
+          ipInfoCache[ip] = data;
+          // Update the state with the fetched data
+          setState(data);
+        })
+        .catch(() => {
+          // Optionally handle errors
+          setState(EMPTY_IP_INFOS);
+        });
+    }
   }, [ip]);
 
   return state;
