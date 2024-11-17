45d94eb74bae170278c662accb9aa3ee47f379ac
Louis Aussedat <aussedat.louis@gmail.com>
2024-11-17 11:55:36 +0100

    ⚡️ (api): Improve iptables rule handling

--- apps/api/src/firewall/firewall.module.ts
+++ apps/api/src/firewall/firewall.module.ts
@@ -1,13 +1,8 @@
 import { Module } from "@nestjs/common";
-import { FirewallCleanupService } from "src/firewall/firewall-cleanup.service";
 import { FirewallEventHandlerService } from "./firewall-event-handler.service";
 import { FirewallProvider } from "./firewall.provider";
 
 @Module({
-  providers: [
-    FirewallProvider,
-    FirewallEventHandlerService,
-    FirewallCleanupService,
-  ],
+  providers: [FirewallProvider, FirewallEventHandlerService],
 })
 export class FirewallModule {}
--- apps/api/src/firewall/iptables-firewall.service.ts
+++ apps/api/src/firewall/iptables-firewall.service.ts
@@ -1,43 +1,83 @@
-import { Logger } from "@nestjs/common";
+import { Logger, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { exec } from "child_process";
 import { promisify } from "util";
 import { Firewall } from "./firewall.interface";
 
-export class IptablesFirewallService implements Firewall {
+export class IptablesFirewallService
+  implements Firewall, OnModuleDestroy, OnModuleInit
+{
   private readonly logger = new Logger(IptablesFirewallService.name);
-  private readonly chain = process.env.BANALIZE_API_FIREWALL_CHAIN ?? "INPUT";
+  private readonly chain = "banalize";
+  private readonly link = process.env.BANALIZE_API_FIREWALL_CHAIN ?? "INPUT";
+  private bannedIps: string[] = [];
+
   private execAsync = promisify(exec);
 
-  // Block an IP using iptables
-  async denyIp(ip: string): Promise<void> {
-    try {
-      // iptables command to drop packets from a specific IP
-      const checkCommand = `iptables -C ${this.chain} -s ${ip}/32 -j REJECT --reject-with icmp-port-unreachable`;
-      const addCommand = `iptables -I ${this.chain} -s ${ip}/32 -j REJECT --reject-with icmp-port-unreachable`;
-
-      // Check if the rule already exists
-      try {
-        await this.execAsync(checkCommand);
-        this.logger.log(`Rule for IP ${ip} already exists, not adding.`);
-      } catch (_err) {
-        // Rule does not exist, so we can add it
-        this.logger.log(`Adding rule for IP ${ip}: ${addCommand}`);
-        await this.execAsync(addCommand);
-      }
-    } catch (error) {
-      this.logger.error(`Failed to add iptables rule: ${error.message}`);
-    }
+  async onModuleDestroy(): Promise<void> {
+    this.logger.log("Cleaning up firewall rules");
+    await this.flushChain();
+    await this.unlinkChain();
+    await this.deleteChain();
+    this.bannedIps = [];
+    this.logger.log("Firewall rules cleaned up");
   }
 
-  // Allow an IP by removing the blocking rule
+  async onModuleInit() {
+    this.logger.log("Initializing firewall");
+    await this.createChain();
+    await this.linkChain();
+    await this.flushChain();
+  }
+
+  async denyIp(ip: string): Promise<void> {
+    if (this.bannedIps.includes(ip)) return;
+
+    const command = `iptables -A ${this.chain} -s ${ip}/32 -j REJECT --reject-with icmp-port-unreachable`;
+    this.executeCommand(command);
+    this.bannedIps.push(ip);
+  }
+
   async allowIp(ip: string): Promise<void> {
+    if (!this.bannedIps.includes(ip)) return;
+
+    const command = `iptables -D ${this.chain} -s ${ip}/32 -j REJECT --reject-with icmp-port-unreachable`;
+    this.executeCommand(command);
+
+    this.bannedIps = this.bannedIps.filter((bannedIp) => bannedIp !== ip);
+  }
+
+  private async linkChain(): Promise<void> {
+    const command = `iptables -I ${this.link} -j ${this.chain}`;
+    this.executeCommand(command);
+  }
+
+  private async unlinkChain(): Promise<void> {
+    const command = `iptables -D ${this.link} -j ${this.chain}`;
+    this.executeCommand(command);
+  }
+
+  private async createChain(): Promise<void> {
+    const command = `iptables -N ${this.chain}`;
+    this.executeCommand(command);
+  }
+
+  private async deleteChain(): Promise<void> {
+    const command = `iptables -X ${this.chain}`;
+    this.executeCommand(command);
+  }
+
+  private async flushChain(): Promise<void> {
+    const command = `iptables -F ${this.chain}`;
+    this.executeCommand(command);
+  }
+
+  private async executeCommand(command: string): Promise<void> {
     try {
-      // iptables command to remove the rule blocking the IP
-      const command = `iptables -D ${this.chain} -s ${ip}/32 -j REJECT --reject-with icmp-port-unreachable`;
-      this.logger.log(`Executing: ${command}`);
+      this.logger.debug(`Executing: ${command}`);
       await this.execAsync(command);
     } catch (error) {
-      this.logger.error(`Failed to remove iptables rule: ${error.message}`);
+      this.logger.error(`Failed to execute: ${command}`);
+      this.logger.error(error);
     }
   }
 }
