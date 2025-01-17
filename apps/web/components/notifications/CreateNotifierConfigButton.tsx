ebf10ac5e824e74a71553a21dfdb96ffd06317be
Louis Aussedat <louis.aussedat@ledger.fr>
2025-01-17 21:46:00 +0100

    🩹 (web): Refresh router on notifier config update

--- apps/web/components/notifications/CreateNotifierConfigButton.tsx
+++ apps/web/components/notifications/CreateNotifierConfigButton.tsx
@@ -1,14 +1,25 @@
 "use client";
 
+import { NotifierConfigDto } from "@banalize/types";
 import { Button, Modal } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { IconPlus } from "@tabler/icons-react";
 import { useNotifications } from "hooks/useNotifications";
+import { useRouter } from "next/navigation";
 import { NotifierConfigForm } from "./NotifierConfigForm";
 
 export const CreateNotifierConfigButton = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const { create } = useNotifications();
+  const router = useRouter();
+
+  const onSubmit = async (dto: NotifierConfigDto) => {
+    const result = await create(dto);
+    router.refresh();
+    close();
+
+    return result;
+  };
 
   return (
     <>
@@ -21,7 +32,7 @@ export const CreateNotifierConfigButton = () => {
       </Button>
 
       <Modal opened={opened} onClose={close} title="Create a new configuration">
-        <NotifierConfigForm onCancel={close} onSubmit={create} />
+        <NotifierConfigForm onCancel={close} onSubmit={onSubmit} />
       </Modal>
     </>
   );
